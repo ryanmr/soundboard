ed83ca86e7e4d1be1cf6fc13ddf85d0ae8cd510a
Ryan Rampersad <ryan.rampersad@gmail.com>
2012-08-21 12:45:48 -0500

    Reorganized structure, added demo.
    Added demo assets under demo/ (minmized for size)
    Changed index to reflect demo/ changes
    Refactored app.js only initialize the scripts.

---
+++ demo/sounds.demo.js
@@ -0,0 +1,18 @@
+/*
+	sounds.js
+	Place all sound information in this file, it is loaded before app.js initializer.
+	Copy this into your soundboard assets since this is just an example.
+*/
+
+var sounds = [
+
+
+	{
+		files: ['demo/hold-on-tweet.mp3'],
+		quote: "Hold on while I tweet this...",
+		author: {name: 'Ryan Rampersad', avatar: 'demo/ryan.jpg'},
+		origin: {name: "Ryan's Blog", link: 'http://blog.ryanrampersad.com/'}
+	},
+
+
+];
