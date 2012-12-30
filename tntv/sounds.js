de06c1befc120d3a5e7904d7de5fb45bdfa822f3
Ryan Rampersad <ryan.rampersad@gmail.com>
2012-12-30 00:51:42 -0600

    Added Nexus directory to this clone.

---
+++ tntv/sounds.js
@@ -0,0 +1,41 @@
+/*
+	sounds.js
+	Place all sound information in this file, it is loaded before app.js initializer.
+*/
+
+var sounds = [
+
+
+	{
+		files: ['assets/audio/have-a-good-one.mp3'],
+		quote: "Have a good one",
+		author: {name: 'Ryan Rampersad', avatar: 'assets/avatar/ryan.jpg', link: 'http://twitter.com/ryanmr'},
+	},
+	{
+		files: ['assets/audio/hold-on-tweet.mp3'],
+		quote: "Hold on while I tweet this...",
+		author: {name: 'Ryan Rampersad', avatar: 'assets/avatar/ryan.jpg'}
+	},
+	{
+		files: ['assets/audio/have-a-good-one.mp3'],
+		quote: "Have a good one",
+		author: {name: 'Ryan Rampersad', avatar: 'assets/avatar/ryan.jpg', link: 'http://twitter.com/ryanmr'},
+	},
+	{
+		files: ['assets/audio/hold-on-tweet.mp3'],
+		quote: "Hold on while I tweet this...",
+		author: {name: 'Ryan Rampersad', avatar: 'assets/avatar/ryan.jpg'}
+	},
+	{
+		files: ['assets/audio/halls-of-the-dead.mp3'],
+		quote: "I have been reading books in the halls of the dead",
+		author: {name: 'Matthew Petschl', avatar: 'assets/avatar/matt.png', link: 'http://twitter.com/matt_petschl'},
+	},
+	{
+		files: ['assets/audio/hold-on-tweet.mp3'],
+		quote: "Hold on while I tweet this...",
+		author: {name: 'Ryan Rampersad', avatar: 'assets/avatar/ryan.jpg'}
+	},
+
+
+];
